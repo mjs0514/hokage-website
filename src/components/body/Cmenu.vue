7d53357f6a651737b921402970130a10b76bf5b6
junyeon <ndkkang2@naver.com>
2019-01-24 00:44:10 +0900

    [준연] Todo List 구현 완료(로컬스토리지 사용) 및 beautify로 자동정렬 수행

--- src/components/body/Cmenu.vue
+++ src/components/body/Cmenu.vue
@@ -1,82 +1,107 @@
 <template>
-  <div class="container">
-    <h2>Todo List</h2>
-    <div class="input-group">
-      <input type="text" class="input-form" placeholder="할 일을 입력하세요~" v-model="todo" v-on:keyup.enter="create(todo)">
-      <button type="button" class="input-button" @click="create(todo)">추가</button>
-    </div>
-    <ul class="list-group">
-      <li class="list-item" v-for="todo in todos">
-      {{todo.name}}
-        <div class="dropdown">
-          <button type="button" @click="remove()" class="remove-button">더 보기</button>
-        </div>
-      </li>
-    </ul>
+<div class="container">
+  <h2>Todo List</h2>
+  <div style="width:353px; height:50px;">
+    <button type="button" style="float:right" @click="removeAll()">초기화</button>
   </div>
+  <div class="input-group">
+    <input type="text" class="input-form" placeholder="할 일을 입력하세요~" v-model="todo" v-on:keyup.enter="createTodo(todo)">
+    <button type="button" class="input-button" @click="createTodo(todo)">추가</button>
+  </div>
+  <ul class="list-group">
+    <li class="list-item" v-for="(todo,index) in todos">
+      {{todo.item}}
+      <button type="button" class="remove-button" @click="remove(index)">삭제</button>
+    </li>
+  </ul>
+</div>
 </template>
 
 <script>
-  export default {
-    name: 'TodoPage',
-    data () {
-      return {
-        todos: JSON.parse(localStorage.getItem('todos'))
+export default {
+  name: 'TodoPage',
+  data() {
+    return {
+      todos: JSON.parse(localStorage.getItem('todos'))
+    }
+  },
+  methods: {
+    createTodo(todo) {
+      if (localStorage.getItem('todos') == null) {
+        localStorage.setItem('todos', JSON.stringify([]));
+      }
+      if (this.todo != null) {
+        var list = JSON.parse(localStorage.getItem('todos'));
+        list.push({
+          item: todo
+        });
+        localStorage.setItem('todos', JSON.stringify(list));
+        window.location.reload();
       }
     },
-    methods:{
-      create() {
-      },
-      remove() {
+    remove(index) {
+      var list = JSON.parse(localStorage.getItem('todos'));
+      list.splice(index, 1);
+      if (list.length == 0) {
+        localStorage.removeItem('todos');
+      } else {
+        localStorage.setItem('todos', JSON.stringify(list));
       }
+      window.location.reload();
+    },
+    removeAll() {
+      localStorage.removeItem('todos');
+      window.location.reload();
     }
   }
+}
 </script>
 
 <style>
-  .container {
-    margin-left: 20px;
-  }
-  .input-group {
-    height: 40px;
-  }
-  .input-form {
-    display: inline-block;
-    float: left;
-    width: 300px;
-    height: 30px;
-    border-color: #F2F2F2;
-    border-width: 1px;
-    border-top-left-radius: 8px;
-    border-bottom-left-radius: 8px;
-  }
-  .input-button {
-    display: inline-block;
-    float: left;
-    width: 50px;
-    height: 34px;
-    background-color: white;
-    border-color: #F2F2F2;
-    border-width: 1px;
-    border-left-width: 0px;
-    border-top-right-radius: 8px;
-    border-bottom-right-radius: 8px;
-  }
-  .list-group {
-    list-style: none;
-    padding-left: 0px;
-    width: 350px;
-  }
-  .list-item {
-    height: 50px;
-  }
-  .button {
-    float: right;
-    display: inline-block;
-  }
-  .remove-button {
-    font-size: 13px;
-    border: none;
-    cursor: pointer;
-  }
+.container {
+  margin-left: 20px;
+}
+
+.input-group {
+  height: 40px;
+}
+
+.input-form {
+  float: left;
+  width: 300px;
+  height: 30px;
+  border-width: 1px;
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+}
+
+.input-button {
+  float: left;
+  width: 50px;
+  height: 34px;
+  background-color: white;
+  border-color: #F2F2F2;
+  border-width: 1px;
+  border-left-width: 0px;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+}
+
+.list-group {
+  list-style: none;
+  padding-left: 0px;
+  width: 350px;
+}
+
+.list-item {
+  height: 50px;
+}
+
+.remove-button {
+  float: right;
+  font-size: 15px;
+  margin-right: 1px;
+  border: none;
+  cursor: pointer;
+}
 </style>
