2f484c836af1b510452e87181262aa91e44341f6
jongho_kim2 <jongho_kim2@tmax.co.kr>
2019-01-28 15:54:15 +0900

    [종호] 부트스트랩 및 삭제 기능 추가

--- src/components/body/Bmenu.vue
+++ src/components/body/Bmenu.vue
@@ -3,118 +3,106 @@
   <div>
     Bmenu 입니다
   </div>
-  <div class="columns is-mobile" id="app">
-      <div class="column is-10 is-offset-1">
-        <!--title-->
-        <p class="title is-2 is-spaced">가계부</p>
 
-        <!--input form-->
-        <div class="field has-addons">
-          <p class="control">
-            <input class="input" type="number" name="amount" v-model="amount" v-on:keyup.enter="createList(amount,des)" placeholder="금액">
-          </p> <!--어떻게 위에 폼에서 des변수를 인식할수 있지?? v-model의 스코프가 어디까지 인가? -->
-          <p class="control is-expanded">
-            <input class="input" type="text" name="description" v-model="des" v-on:keyup.enter="createList(amount,des)" placeholder="내역">
-          </p>
-          <p class="control">
-            <button class="button is-primary" v-on:click="createList(amount,des)">저장</button>
-          </p>
+  <button type="button" class="btn btn-primary" autocomplate="off" data-loading-text="jquery with bootstrap" v-on:click="clickBtn">버튼</button>
+
+  <div>
+    <div>
+      <!--title-->
+      <p>가계부</p>
+      <!--input form-->
+      <div class="row" >
+        <div class="col-md-4">
+        <input  type="number" name="amount" v-model="amount" v-on:keyup.enter="createList(amount,des)" placeholder="금액">
+        <!--어떻게 위에 폼에서 des변수를 인식할수 있지?? v-model의 스코프가 어디까지 인가? -->
         </div>
-
-        <!--dashboard-->
-        <div class="columns">
-          <div class="column">
-            <article class="message is-success">
-              <div class="message-header">
-                <p>Income</p>
-              </div>
-              <div class="message-body">
-              </div>
-            </article>
-          </div>
-          <div class="column">
-            <article class="message is-danger">
-              <div class="message-header">
-                <p>Outcome</p>
-              </div>
-              <div class="message-body">
-              </div>
-            </article>
-          </div>
-          <div class="column">
-            <article class="message is-info">
-              <div class="message-header">
-                <p>Total</p>
-              </div>
-              <div class="message-body">
-              </div>
-            </article>
-          </div>
-        </div>
-
-        <!--contents-->
-        <table border=1 class="table">
-          <thead>
-            <tr>
-              <th>번호</th>
-              <th>날짜</th>
-              <th>범주</th>
-              <th>금액</th>
-              <th>내용</th>
-              <th>변경</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(account,index) in accountLists">
-              <td>{{index}}</td>
-              <td>{{account.day}}</td>
-              <td>{{account.category}}</td>
-              <td>{{account.amount}}</td>
-              <td>{{account.description}}</td>
-              <td>
-                <button class="button is-danger">삭제</button>
-                <button class="button is-light">수정</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div class="col-md-4">
+        <input class="form-control" type="text" name="description" v-model="des" v-on:keyup.enter="createList(amount,des)" placeholder="내역">
       </div>
+        <div class="col-md-4">
+        <button class="input-group-text" v-on:click="createList(amount,des)">저장</button> <!--보낼 값이 늘어나면 매번 파라미터를 늘려야되는가?-->
+      </div >
+      </div>
+
+      <div class="jh-content"> <!--왜 여기에 스타일을 주면 안먹히나? 부트스트랩은 태그에 값주면 안되나?-->
+      <!--부트스트랩css보다 나중에 vue 스타일을 로딩할려면 어떻게해야되나? -->
+      <!--contents-->
+      <table border=1>
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>날짜</th>
+            <th>범주</th>
+            <th>금액</th>
+            <th>내용</th>
+            <th>변경</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(account,index) in accountLists"> <!--js에서 태그를 만드는 방식과 비교해서 뭐가 더 좋은가?-->
+            <td>{{index}}</td>
+            <td>{{account.day}}</td>
+            <td>{{account.category}}</td>
+            <td>{{account.amount}}</td>
+            <td>{{account.description}}</td>
+            <td>
+              <button v-on:click="deleteList(index)">삭제</button>
+              <button >수정</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      </div>
+
     </div>
+  </div>
 </body>
 </template>
 
 <script>
 export default {
-name : 'AccountBook',
-data () {
-  return {
-    accountLists: [
-      {
+  name: 'AccountBook',
+  data() {
+    return {
+      accountLists: [{
         day: '2019.1.27',
         category: '수입',
-        amount:'1000',
-        description:"월급"
-      },{
+        amount: '1000',
+        description: "월급"
+      }, {
         day: '2019.1.28',
         category: '지출',
-        amount:'500',
-        description:"저녁"
-      },
-    ]
+        amount: '500',
+        description: "저녁"
+      }, ]
+    }
+  },
+  methods: {
+    createList: function(amount, des) {
+      this.accountLists.push({
+        amount: amount,
+        description: des
+      });
+    },
+    deleteList: function(index) {
+      this.accountLists.splice(index,1);
+    },
+    clickBtn(event) {
+      console.log('1');
+    }
   }
-},
-methods:{
-  createList : function(amount,des){
-      this.accountLists.push({amount:amount, description:des});
-  }
-}
 
 }
 </script>
 
 <style>
-
-  td {
-    padding: 0px 20px;
-  }
+td {
+  padding: 0px 20px;
+}
+th {
+  padding: 0px 20px;
+}
+.jh-content {
+  margin-top:20px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
+import 'expose-loader?$!expose-loader?jQuery!jquery'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 new Vue({
   el: '#app',
