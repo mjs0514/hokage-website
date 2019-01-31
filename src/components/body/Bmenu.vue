6e5244303cb24b36233a3f103296ff2a8cd40185
jongho_kim2 <jongho_kim2@tmax.co.kr>
2019-01-31 14:56:53 +0900

    [종호] 고정형 네비바 추가

--- src/components/body/Bmenu.vue
+++ src/components/body/Bmenu.vue
@@ -4,28 +4,17 @@
     Bmenu 입니다
   </div>
 
-  <button type="button" class="btn btn-primary" autocomplate="off" data-loading-text="jquery with bootstrap" v-on:click="clickBtn">버튼</button>
-
   <div>
     <div>
       <!--title-->
       <p>가계부</p>
       <!--input form-->
-      <div class="row" >
-        <div class="col-md-4">
-        <input  type="number" name="amount" v-model="amount" v-on:keyup.enter="createList(amount,des)" placeholder="금액">
-        <!--어떻게 위에 폼에서 des변수를 인식할수 있지?? v-model의 스코프가 어디까지 인가? -->
-        </div>
-        <div class="col-md-4">
-        <input class="form-control" type="text" name="description" v-model="des" v-on:keyup.enter="createList(amount,des)" placeholder="내역">
+      <div class="navbar fixed-bottom row" > <!--부트스트랩을 이용해 고정형 네비바를 만들고 싶을때-->
+        <input class="col-md-5" type="number" name="amount" v-model="amount" v-on:keyup.enter="createList(amount,des)" placeholder="금액">
+        <input class="col-md-5 form-control" type="text" name="description" v-model="des" v-on:keyup.enter="createList(amount,des)" placeholder="내역">
+        <button class="col-md-2 input-group-text" v-on:click="createList(amount,des)">저장</button> <!--보낼 값이 늘어나면 매번 파라미터를 늘려야되는가?-->
       </div>
-        <div class="col-md-4">
-        <button class="input-group-text" v-on:click="createList(amount,des)">저장</button> <!--보낼 값이 늘어나면 매번 파라미터를 늘려야되는가?-->
-      </div >
-      </div>
-
-      <div class="jh-content"> <!--왜 여기에 스타일을 주면 안먹히나? 부트스트랩은 태그에 값주면 안되나?-->
-      <!--부트스트랩css보다 나중에 vue 스타일을 로딩할려면 어떻게해야되나? -->
+      <div class="jh-content">
       <!--contents-->
       <table border=1>
         <thead>
@@ -39,7 +28,7 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(account,index) in accountLists"> <!--js에서 태그를 만드는 방식과 비교해서 뭐가 더 좋은가?-->
+          <tr v-for="(account,index) in accountLists">
             <td>{{index}}</td>
             <td>{{account.day}}</td>
             <td>{{account.category}}</td>
@@ -95,7 +84,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped> /* scoped를 붙이면 해당하는 컴포넌트에만 style을 적용하는것 */
 td {
   padding: 0px 20px;
 }
@@ -104,5 +93,21 @@ th {
 }
 .jh-content {
   margin-top:20px;
+}
+
+@media (max-width:400px) { /* 브라우저 크기 0~400일때 적용되는 구문 */
+  p {
+    background-color:blue;
+  }
+}
+@media (min-width:401px) and (max-width:600px){
+  p {
+    background-color:green;
+  }
+}
+@media (min-width:601px) and (max-width:768px){
+  p {
+    background-color:red;
+  }
 }
 </style>
