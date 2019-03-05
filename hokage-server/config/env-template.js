e5015b1c91c933ced1f660e70ef997f1b57dda8b
junyeon_lee <junyeon_lee@tmax.co.kr>
2019-03-05 13:48:04 +0900

    [준연] hokage-server에 바벨 추가 및 DB 서버 접속 템플릿 작성

---
+++ hokage-server/config/env-template.js
@@ -0,0 +1,15 @@
+/*
+port는 int이므로 ''없이 숫자 입력
+*/
+
+'use strict'
+
+module.exports = {
+  conn : require('mysql').createConnection({
+    host : '',
+    user : '',
+    password : '',
+    port : ,
+    database : ''
+  })
+}
