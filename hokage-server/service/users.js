01f02a5dd48ca330d39b300bdf8ce3efa5ecc21e
sungsoo <ksungboy@gmail.com>
2019-03-08 13:23:50 +0900

    [server] users 서비스 모듈 - 단건, 다건 조회 서비스 설계 및 개발

---
+++ hokage-server/service/users.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+var env = require('../config/env.js');
+
+
+/*
+단건은 pk를 필수적으로 입력받도록 하는 서비스여야 한다. 즉 path parameter로 입력을 처리하게 하면 됨
+다건은 전체인 경우와 일부인 경우로 나뉘는데 이 때 일부를 검색하는 것은 옵션이기 때문에 query string으로 처리
+
+전체인지 일부인지를 구분하는 query string parameter를 둬도 되지만 그냥 아무 파라미터가 안들어오면 전체리스트 라고 판단하는게 더 자연스러움
+만약 파라미터로 구분하려면 if문이 추가될 것이고 이것을 없애기 위해 서비스를 분리하게 되면 url이 복잡해진다.
+*/
+
+
+/* 다건 사용자 조회
+method : GET
+url : /service/users
+params : none
+query string : region, password
+*/
+// 단점 : 쿼리스트링 파라미터가 늘어나면 날 수록 if문이 많아짐 -> 쿼리문으로 해결하고 싶었는데 못찾음
+router.get('/', function(req, res, next) {
+
+  let query;
+  if(req.query.region === undefined) {
+    console.log('region undefined')
+    query = `select * from UserInfo`;
+  } else {
+    console.log(req.query.region);
+    query = `select * from UserInfo where region = "${req.query.region}"`;
+  }
+
+  env.conn.query(query, function(error, data){
+    console.log(data);
+    console.log(error);
+  });
+  res.send('다건 사용자 조회 완료\n');
+});
+
+/* 단건 사용자 조회
+method : GET
+url : /service/users/
+params : id
+query string : none
+*/
+router.get('/:id', function(req, res, next) {
+  env.conn.query(`select * from UserInfo where id="${req.params.id}"`, function(error, data){
+    console.log(data);
+    console.log(error);
+  });
+  res.send('단건 사용자 조회 완료\n');
+});
+
+module.exports = router;
